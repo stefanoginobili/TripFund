44cfa77c95dde9c4261e89f74eccfa3f0fdda2f7
Stefano Ginobili <stefano.ginobili@gmail.com>
2026-04-11 14:02:07 +0200

    chore: configure build environments and add project branding
    - Add logo assets in PNG and SVG formats to documentation.
    - Update Google Auth client IDs for Android and iOS development environments.
    - Implement conditional build configurations in project file to differentiate between development and production app identifiers and titles.

--- src/TripFund.App/Services/GoogleAuthConfiguration.cs
+++ src/TripFund.App/Services/GoogleAuthConfiguration.cs
@@ -13,13 +13,13 @@ public class GoogleAuthConfiguration : IGoogleAuthConfiguration
         {
 #if ANDROID
 #if DEBUG
-            return "ANDROID_CLIENT_ID_DEV";
+            return "980516715181-o2u5ona8nnh2v46l1spbm7i854bujp8v.apps.googleusercontent.com";
 #else
             return "ANDROID_CLIENT_ID_PROD";
 #endif
 #elif IOS
 #if DEBUG
-            return "IOS_CLIENT_ID_DEV";
+            return "980516715181-b4g5jlbe72uak9518dm1ej9h2l844038.apps.googleusercontent.com";
 #else
             return "IOS_CLIENT_ID_PROD";
 #endif
